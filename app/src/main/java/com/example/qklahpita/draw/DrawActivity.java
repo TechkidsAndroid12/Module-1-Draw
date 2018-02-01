3a1b781b0a8e136a696e5b049288d901e29e9e07
Qk Lahpita <techkids.a12@gmail.com>
2018-02-01 10:58:38 +0700

    Homework Session 3

--- app/src/main/java/com/example/qklahpita/draw/DrawActivity.java
+++ app/src/main/java/com/example/qklahpita/draw/DrawActivity.java
@@ -1,11 +1,14 @@
 package com.example.qklahpita.draw;
 
 import android.content.DialogInterface;
+import android.support.annotation.IdRes;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 import com.flask.colorpicker.ColorPickerView;
 import com.flask.colorpicker.OnColorSelectedListener;
@@ -17,6 +20,7 @@ public class DrawActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = "DrawActivity";
     private ImageView ivColor;
     private ImageView ivDone;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +33,31 @@ public class DrawActivity extends AppCompatActivity implements View.OnClickListe
     private void setupUI() {
         ivColor = findViewById(R.id.iv_color);
         ivDone = findViewById(R.id.iv_done);
+        radioGroup = findViewById(R.id.gr_pen_size);
+        radioGroup.check(R.id.rb_medium);
 
         ivColor.setOnClickListener(this);
         ivDone.setOnClickListener(this);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+                switch (i) {
+                    case R.id.rb_thin: {
+
+                        break;
+                    }
+                    case R.id.rb_medium: {
+
+                        break;
+                    }
+                    case R.id.rb_strong: {
+
+                        break;
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -41,13 +67,13 @@ public class DrawActivity extends AppCompatActivity implements View.OnClickListe
                 ColorPickerDialogBuilder
                         .with(this)
                         .setTitle("Choose color")
-                        .initialColor(R.color.colorAccent)
+                        .initialColor(ContextCompat.getColor(this, R.color.colorAccent))
                         .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
                         .density(12)
                         .setOnColorSelectedListener(new OnColorSelectedListener() {
                             @Override
                             public void onColorSelected(int selectedColor) {
-                                Log.d(TAG, "onColorSelected: " + selectedColor);
+
                             }
                         })
                         .setPositiveButton("Ok", new ColorPickerClickListener() {
